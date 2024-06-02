6c9258d4875e649615c9460a868ad2fdf599098b
paratellaDev <paratelladevmarcos@gmail.com>
2024-06-02 18:34:54 -0300

    Criação  de um mock para componentes

---
+++ src/components-mock.js
@@ -0,0 +1,14 @@
+{
+    "vertical_cards": [
+        {
+            "title": "Vertical Card",
+            "content": "Vertical Card",
+            "image": {
+                "url": "https://picsum.photos/200/300",
+                "alt": "Vertical Card",
+                "link": "https://picsum.photos/200/300",
+            },
+            "publish_date": "02 de maio de 2024."
+        }
+    ]
+}
