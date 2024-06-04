f43924d7cfaa86c78306a14b1a7d2e74410a0437
jsvivis <0001039168@senaimgaluno.com.br>
2024-06-04 15:42:29 -0300

    atualização de dados no footer

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -7,13 +7,13 @@ export function Footer(props) {
     <Flex as="footer" bg="gray.700" color="white" p={2} textAlign="center" direction="column" alignItems="center" position="fixed" bottom={0} width="100%">
       <Text>{props.nomeFooter}</Text>
       <Flex mt={2} justifyContent="center" width="100%">
-<<<<<<< HEAD
+
         <SocialLink name="Marcos Paratella" github="https://github.com/marcosparatella" linkedin="https://linkedin.com/in/marcosparatella" />
         <SocialLink name="Viviane Santos" github="https://github.com/jsvivis" linkedin="https://www.linkedin.com/in/viviane-santos-a6a69127b"/>
 =======
         <SocialLink name="Marcos Paratella" github="https://github.com/devParatella" linkedin="https://www.linkedin.com/in/marcos-paratella-74b291239/" />
-        <SocialLink name="Viviane Santos" github="https://github.com/vivianeds" linkedin="https://linkedin.com/in/vivianeds" />
->>>>>>> 728e2140a6cbc60e78bee2487567aa332f047058
+        <SocialLink name="Viviane Santos" github="https://github.com/jsvivis" linkedin="https://www.linkedin.com/in/viviane-santos-a6a69127b" />
+
         <SocialLink name="Mayra Pacheco" github="https://github.com/mayrapacheco" linkedin="https://linkedin.com/in/mayrapacheco" />
       </Flex>
     </Flex>
